e27bdcff527f9c1c140f6874e2cc7d24332b60dc
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 13:48:33 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,21 @@
+// Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
+// তোমার Firebase Config এখানে বসাবে
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
