8b1b78b0bf2880c57777629dca15accaa06bb2d2
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 19:24:24 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const totalTeams = document.getElementById("totalTeams");
+const totalGroups = document.getElementById("totalGroups");
+const totalMatches = document.getElementById("totalMatches");
+
+// Live Team Count
+onSnapshot(collection(db, "teams"), (snap) => {
+    totalTeams.textContent = snap.size;
+});
+
+// Live Group Count
+onSnapshot(collection(db, "groups"), (snap) => {
+    totalGroups.textContent = snap.size;
+});
+
+// Live Match Count
+onSnapshot(collection(db, "fixtures"), (snap) => {
+    totalMatches.textContent = snap.size;
+});
