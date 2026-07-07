5a5b01f6049137991557ed28e34eed183e6f71bb
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 18:18:01 +0600

    Update public-fixtures.js

--- js/public-fixtures.js
+++ js/public-fixtures.js
@@ -1,1 +1,46 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+
+const fixtureList = document.getElementById("fixtureList");
+
+
+// Live Fixture View
+
+onSnapshot(collection(db,"fixtures"), (snapshot)=>{
+
+
+    fixtureList.innerHTML = "";
+
+
+    snapshot.forEach((match)=>{
+
+
+        const data = match.data();
+
+
+        fixtureList.innerHTML += `
+
+        <tr>
+
+        <td>${data.group}</td>
+
+        <td>${data.home}</td>
+
+        <td>${data.away}</td>
+
+        <td>${data.status}</td>
+
+        </tr>
+
+        `;
+
+
+    });
+
+
+});
