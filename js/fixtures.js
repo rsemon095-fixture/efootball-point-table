a010b75096fa4b5ee0a3b0744d868febed55312d
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 17:23:29 +0600

    Update fixtures.js

--- js/fixtures.js
+++ js/fixtures.js
@@ -61,3 +61,81 @@ function createFixtures(teams){
     return matches;
 
 }
+// Generate Fixture Button
+
+generateBtn.addEventListener("click", async () => {
+
+    const allGroups = await loadAssignments();
+
+    for (const group in allGroups) {
+
+        const teams = allGroups[group];
+
+        const matches = createFixtures(teams);
+
+
+        for (const match of matches) {
+
+            await addDoc(collection(db, "fixtures"), {
+
+                group: group,
+
+                home: match.home,
+
+                away: match.away,
+
+                status: "Upcoming",
+
+                createdAt: Date.now()
+
+            });
+
+        }
+
+    }
+
+    alert("Fixtures Generated Successfully");
+
+});
+
+
+// Live Fixture List
+
+onSnapshot(collection(db,"fixtures"), (snapshot)=>{
+
+
+    fixtureList.innerHTML="";
+
+
+    let i = 1;
+
+
+    snapshot.forEach((doc)=>{
+
+
+        const data = doc.data();
+
+
+        fixtureList.innerHTML += `
+
+        <tr>
+
+        <td>${i++}</td>
+
+        <td>${data.group}</td>
+
+        <td>${data.home}</td>
+
+        <td>${data.away}</td>
+
+        <td>${data.status}</td>
+
+        </tr>
+
+        `;
+
+
+    });
+
+
+});
