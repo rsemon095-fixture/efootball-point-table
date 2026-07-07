f55b935a13220b9745965020ab176d655207e74c
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 15:08:08 +0600

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,21 @@
+import { auth } from "./firebase.js";
 
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+// Login না থাকলে Login Page-এ পাঠাবে
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        window.location.href = "login.html";
+    }
+});
+
+// Logout
+logoutBtn.addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "login.html";
+});
