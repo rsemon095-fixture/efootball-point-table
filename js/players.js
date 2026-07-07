d7392cd9e46169eb96e2f1d6aba4e558f082e57c
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 19:46:02 +0600

    Update players.js

--- js/players.js
+++ js/players.js
@@ -1,1 +1,78 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const playerName = document.getElementById("playerName");
+const jersey = document.getElementById("jersey");
+const position = document.getElementById("position");
+const team = document.getElementById("team");
+const addPlayerBtn = document.getElementById("addPlayerBtn");
+const playerList = document.getElementById("playerList");
+
+// Load Teams
+async function loadTeams() {
+    const snap = await getDocs(collection(db, "teams"));
+
+    snap.forEach(doc => {
+        const option = document.createElement("option");
+        option.value = doc.data().name;
+        option.textContent = doc.data().name;
+        team.appendChild(option);
+    });
+}
+
+loadTeams();
+
+// Add Player
+addPlayerBtn.addEventListener("click", async () => {
+
+    if (
+        playerName.value === "" ||
+        jersey.value === "" ||
+        position.value === "" ||
+        team.value === ""
+    ) {
+        alert("সব তথ্য পূরণ করুন");
+        return;
+    }
+
+    await addDoc(collection(db, "players"), {
+        name: playerName.value,
+        jersey: jersey.value,
+        position: position.value,
+        team: team.value
+    });
+
+    playerName.value = "";
+    jersey.value = "";
+    position.value = "";
+    team.value = "";
+
+});
+
+// Live Player List
+onSnapshot(collection(db, "players"), (snap) => {
+
+    playerList.innerHTML = "";
+
+    snap.forEach(doc => {
+
+        const p = doc.data();
+
+        playerList.innerHTML += `
+        <div class="player-card">
+            <h3>${p.name}</h3>
+            <p>Team : ${p.team}</p>
+            <p>Position : ${p.position}</p>
+            <p>Jersey : ${p.jersey}</p>
+        </div>
+        `;
+
+    });
+
+});
