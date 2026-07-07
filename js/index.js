172fd8070c9d26245a63b6cb567236984f8ff46e
rsemon095-fixture <rsemon095@gmail.com>
2026-07-08 03:06:32 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import { db } from "./firebase.js";
 
 import {
   collection,
+  doc,
   onSnapshot
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
@@ -22,4 +23,37 @@ onSnapshot(collection(db, "groups"), (snap) => {
 // Live Match Count
 onSnapshot(collection(db, "fixtures"), (snap) => {
     totalMatches.textContent = snap.size;
+});
+
+
+// =========================
+// Homepage Upcoming Match
+// =========================
+
+const team1Name = document.getElementById("team1Name");
+const team2Name = document.getElementById("team2Name");
+const team1Logo = document.getElementById("team1Logo");
+const team2Logo = document.getElementById("team2Logo");
+const matchDate = document.getElementById("matchDate");
+const matchTime = document.getElementById("matchTime");
+const matchDeadline = document.getElementById("matchDeadline");
+const matchStatus = document.getElementById("matchStatus");
+
+onSnapshot(doc(db,"fixtures","nextMatch"),(docSnap)=>{
+
+if(!docSnap.exists()) return;
+
+const data = docSnap.data();
+
+team1Name.textContent=data.team1;
+team2Name.textContent=data.team2;
+
+team1Logo.src=data.team1Logo;
+team2Logo.src=data.team2Logo;
+
+matchDate.textContent=data.date;
+matchTime.textContent=data.time;
+matchDeadline.textContent=data.deadline;
+matchStatus.textContent=data.status;
+
 });
