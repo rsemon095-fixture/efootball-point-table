377eefea0d087b705c00175f3db9f0bf6c07cda9
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 20:23:36 +0600

    Update admin-players.js

--- admin-players.js
+++ admin-players.js
@@ -1,1 +1,105 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const playerName = document.getElementById("playerName");
+const jersey = document.getElementById("jersey");
+const position = document.getElementById("position");
+const team = document.getElementById("team");
+const addPlayerBtn = document.getElementById("addPlayerBtn");
+const playerList = document.getElementById("playerList");
+
+// Load Teams
+async function loadTeams() {
+    const snap = await getDocs(collection(db, "teams"));
+
+    team.innerHTML = `<option value="">Select Team</option>`;
+
+    snap.forEach(teamDoc => {
+        const option = document.createElement("option");
+        option.value = teamDoc.data().name;
+        option.textContent = teamDoc.data().name;
+        team.appendChild(option);
+    });
+}
+
+loadTeams();
+
+// Add Player
+addPlayerBtn.addEventListener("click", async () => {
+
+    if (
+        playerName.value === "" ||
+        jersey.value === "" ||
+        position.value === "" ||
+        team.value === ""
+    ) {
+        alert("সব তথ্য পূরণ করুন");
+        return;
+    }
+
+    await addDoc(collection(db, "players"), {
+        name: playerName.value,
+        jersey: jersey.value,
+        position: position.value,
+        team: team.value
+    });
+
+    playerName.value = "";
+    jersey.value = "";
+    position.value = "";
+    team.value = "";
+
+});
+
+// Live Player List
+onSnapshot(collection(db, "players"), (snap) => {
+
+    playerList.innerHTML = "";
+
+    snap.forEach(playerDoc => {
+
+        const p = playerDoc.data();
+
+        playerList.innerHTML += `
+        <div class="player-card">
+
+            <h3>${p.name}</h3>
+
+            <p>Team : ${p.team}</p>
+
+            <p>Position : ${p.position}</p>
+
+            <p>Jersey : ${p.jersey}</p>
+
+            <div class="action-btn">
+
+                <button class="delete"
+                onclick="deletePlayer('${playerDoc.id}')">
+                🗑 Delete
+                </button>
+
+            </div>
+
+        </div>
+        `;
+
+    });
+
+});
+
+// Delete Player
+window.deletePlayer = async (id) => {
+
+    if (!confirm("Delete this player?")) return;
+
+    await deleteDoc(doc(db, "players", id));
+
+};
