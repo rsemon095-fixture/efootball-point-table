bd4a25ea80953c0228e02a2048df8102fb003189
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 14:08:10 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -10,7 +10,23 @@ const firebaseConfig = {
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_PROJECT.firebasestorage.app",
   messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  appId: "// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBc4A_OwclBuYzs4L4-2btw8clIlEXuLCo",
+  authDomain: "efootball-bd-club-2026-26e69.firebaseapp.com",
+  projectId: "efootball-bd-club-2026-26e69",
+  storageBucket: "efootball-bd-club-2026-26e69.firebasestorage.app",
+  messagingSenderId: "585390477677",
+  appId: "1:585390477677:web:c341c5ff53c78497b28a64"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);"
 };
 
 // Initialize Firebase
