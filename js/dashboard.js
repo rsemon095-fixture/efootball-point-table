0e0bbd157f2ebdcb4a21e1e3ccc883103c50507f
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 19:07:41 +0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,43 @@
+import { db, auth } from "./firebase.js";
 
+import {
+  collection,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+import {
+  signOut
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const teamCount = document.getElementById("teamCount");
+const groupCount = document.getElementById("groupCount");
+const fixtureCount = document.getElementById("fixtureCount");
+const playerCount = document.getElementById("playerCount");
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+// Live Team Count
+onSnapshot(collection(db, "teams"), (snap) => {
+    teamCount.textContent = snap.size;
+});
+
+// Live Group Count
+onSnapshot(collection(db, "groups"), (snap) => {
+    groupCount.textContent = snap.size;
+});
+
+// Live Fixture Count
+onSnapshot(collection(db, "fixtures"), (snap) => {
+    fixtureCount.textContent = snap.size;
+});
+
+// Live Player Count
+onSnapshot(collection(db, "players"), (snap) => {
+    playerCount.textContent = snap.size;
+});
+
+// Logout
+logoutBtn.addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "login.html";
+});
