7c3573c0ed15a42cd6275520fd8c2b8cbfcdb745
rsemon095-fixture <rsemon095@gmail.com>
2026-07-07 14:23:27 +0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,29 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const error = document.getElementById("error");
+
+loginBtn.addEventListener("click", async () => {
+
+    error.innerText = "";
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email.value,
+            password.value
+        );
+
+        window.location.href = "admin.html";
+
+    } catch (err) {
+
+        error.innerText = "Invalid Email or Password";
+
+    }
+
+});
