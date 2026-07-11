15c441adc8a67c1659c726cde35c2d9fa1782e6a
rsemon095-fixture <rsemon095@gmail.com>
2026-07-11 13:22:03 +0600

    Enhance maintenance.js with status indicators

--- js/maintence.js
+++ js/maintence.js
@@ -1,6 +1,10 @@
 import { rtdb } from "./firebase.js";
-alert("maintenance.js Loaded");
-import { ref, set, get } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
+import {
+  ref,
+  get,
+  set
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
 
 const onBtn = document.getElementById("onBtn");
 const offBtn = document.getElementById("offBtn");
@@ -9,33 +13,67 @@ const status = document.getElementById("status");
 const maintenanceRef = ref(rtdb, "system/maintenance");
 
 async function loadStatus() {
+
+  try {
+
     const snap = await get(maintenanceRef);
 
     if (snap.exists() && snap.val() === true) {
-        status.innerHTML = "Current Status : ON";
-        status.style.color = "#00ff66";
+
+      status.innerHTML = "🟢 Current Status : ON";
+      status.style.color = "#00ff66";
+
     } else {
-        status.innerHTML = "Current Status : OFF";
-        status.style.color = "#ff4444";
+
+      status.innerHTML = "🔴 Current Status : OFF";
+      status.style.color = "#ff4444";
+
     }
+
+  } catch (e) {
+
+    alert(e.message);
+
+  }
+
 }
 
-onBtn.onclick = async () => {
+onBtn.onclick = async function () {
+
+  try {
+
     await set(maintenanceRef, true);
 
-    status.innerHTML = "Current Status : ON";
+    status.innerHTML = "🟢 Current Status : ON";
     status.style.color = "#00ff66";
 
     alert("Maintenance Mode Enabled");
+
+  } catch (e) {
+
+    alert(e.message);
+
+  }
+
 };
 
-offBtn.onclick = async () => {
+offBtn.onclick = async function () {
+
+  try {
+
     await set(maintenanceRef, false);
 
-    status.innerHTML = "Current Status : OFF";
+    status.innerHTML = "🔴 Current Status : OFF";
     status.style.color = "#ff4444";
 
     alert("Maintenance Mode Disabled");
+
+  } catch (e) {
+
+    alert(e.message);
+
+  }
+
 };
 
 loadStatus();
